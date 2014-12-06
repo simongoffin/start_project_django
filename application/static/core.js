d9f4d65f48d2c04f87e5e483ea564516344d12ee
Goffin Simon <simon.goffin@outlook.com>
2014-12-06 17:56:51 +0100

    [IMP] Computing in js.

--- application/static/core.js
+++ application/static/core.js
@@ -1,6 +1,19 @@
 
 $( document ).ready(function() {
-    $( ".xxx" ).on( "click", function() {
-        console.log( "You clicked!" );
+    $( ".run_js" ).on( "click", function() {
+        var xx = parseInt($('.x_val').val());
+        var yy = parseInt($('.y_val').val());
+        if(xx && yy){
+            var zz = xx+yy;
+            $('.solution').text(xx+'+'+yy+'='+zz);
+        }
+        else{
+            $('.solution').text('Bad value');
+        }
+    });
+    $( ".clear_js" ).on( "click", function() {
+        $('.x_val').val('');
+        $('.y_val').val('');
+        $('.solution').text('');
     });
 });
